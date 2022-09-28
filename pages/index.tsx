81a974452770ff40f5a2c43f903f54c4cc79d5c0
syoutarou imamura <syoutarouimamura@gmail.com>
2022-09-28 13:32:47 +0900

    レッスン一覧表示 ・　csv吐き出し

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,117 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { db } from "../src/lib/firebase";
-import { collection, doc, setDoc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  setDoc,
+  getDocs,
+  DocumentData,
+} from "firebase/firestore";
 import styles from "../styles/Home.module.css";
+import Papa from "papaparse";
+import Encoding from "encoding-japanese";
 
 const Home: NextPage = () => {
+  const japanStandardTime = new Date().toLocaleString("ja-JP", {
+    timeZone: "Asia/Tokyo",
+  });
+  const now = new Date(japanStandardTime);
+  const [month, setMonth] = useState<number>(now.getFullYear());
+  const [yaer, setYaer] = useState<number>(now.getHours() + 1);
+  const selectYear = [now.getFullYear(), now.getFullYear() + 1];
   useEffect(() => {
-    getUsers();
+    // getLessons();
   }, []);
 
-  const getUsers = async () => {
+  const getPeriodLessons = () => {
+    const lessons = getLessons();
+  };
+
+  const getLessons = async () => {
+    const lessonList: DocumentData[] = [];
     const userRef = getDocs(collection(db, "lessons"));
     (await userRef).forEach((doc) => {
-      console.log(doc.data());
+      lessonList.push(doc.data());
     });
+    return lessonList;
   };
+
+  const getInstructorBankData = async () => {
+    const instructorList: DocumentData[] = [];
+    const userRef = getDocs(collection(db, "instructor"));
+    (await userRef).forEach((doc, index) => {
+      instructorList.push([
+        index + 1,
+        doc.data().bank.code,
+        doc.data().bank.brunch,
+        doc.data().bank.type,
+        doc.data().bank.number,
+        doc.data().bank.name,
+        `00${index + 1}`,
+      ]);
+    });
+    return instructorList;
+  };
+
+  const csvExport = async () => {
+    const instructorList = await getInstructorBankData();
+    const config = {
+      delimiter: ",", // 区切り文字
+      header: true, // キーをヘッダーとして扱う
+      newline: "\r\n", // 改行
+    };
+
+    // 区切り文字へ変換
+    const delimiterString = Papa.unparse(instructorList, config);
+
+    // blodへの変換
+    const strArray = Encoding.stringToCode(delimiterString);
+    const convertedArray = Encoding.convert(strArray, "UTF8", "UNICODE");
+    const UintArray = new Uint8Array(convertedArray);
+    const blob = new Blob([UintArray], { type: "text/csv" });
+
+    // download
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = `back.csv`;
+    link.click();
+  };
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>ログイン</main>
+      <main className={styles.main}>
+        ログイン
+        <button onClick={csvExport}>全銀データエクスポート</button>
+        <select
+          value={yaer}
+          onChange={(e) => {
+            setYaer(Number(e.target.value));
+          }}
+        >
+          <option>年を選択</option>
+          {selectYear.map((year) => (
+            <option value={year} key={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+        <select
+          value={month}
+          onChange={(e) => {
+            setMonth(Number(e.target.value));
+          }}
+        >
+          <option>月を選択</option>
+          {[...Array(12)].map((_, index) => (
+            <option value={index + 1} key={index}>
+              {index + 1}
+            </option>
+          ))}
+        </select>
+      </main>
     </div>
   );
 };
--- pages/lessons/index.tsx
+++ pages/lessons/index.tsx
@@ -1,27 +1,83 @@
 import type { NextPage } from "next";
-import { useEffect } from "react";
-import { db } from "../../src/lib/firebase";
-import { collection, doc, setDoc, getDocs } from "firebase/firestore";
-import styles from "../styles/Home.module.css";
 
-const LessonsPage: NextPage = () => {
+import { useEffect, useState } from "react";
+import { db } from "../../src/lib/firebase";
+import {
+  collection,
+  doc,
+  setDoc,
+  getDocs,
+  DocumentData,
+} from "firebase/firestore";
+import styles from "../../styles/Home.module.css";
+
+const Lesson: NextPage = () => {
+  const japanStandardTime = new Date().toLocaleString("ja-JP", {
+    timeZone: "Asia/Tokyo",
+  });
+  const now = new Date(japanStandardTime);
+  const [month, setMonth] = useState<number>(now.getFullYear());
+  const [yaer, setYaer] = useState<number>(now.getHours() + 1);
+  const [lessons, setLessons] = useState<any>([]);
+  const selectYear = [now.getFullYear(), now.getFullYear() + 1];
   useEffect(() => {
-    getUsers();
+    getLessons();
   }, []);
 
-  const getUsers = async () => {
+  const getPeriodLessons = () => {
+    const lessons = getLessons();
+  };
+
+  const getLessons = async () => {
+    const lessonList: DocumentData[] = [];
     const userRef = getDocs(collection(db, "lessons"));
     (await userRef).forEach((doc) => {
-      console.log(doc.data());
+      lessonList.push(doc.data());
     });
+    setLessons(lessonList);
   };
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div>レッスン一覧</div>
+        <h1>レッスン一覧</h1>
+
+        <select
+          value={yaer}
+          onChange={(e) => {
+            setYaer(Number(e.target.value));
+          }}
+        >
+          <option>年を選択</option>
+          {selectYear.map((year) => (
+            <option value={year} key={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+        <select
+          value={month}
+          onChange={(e) => {
+            setMonth(Number(e.target.value));
+          }}
+        >
+          <option>月を選択</option>
+          {[...Array(12)].map((_, index) => (
+            <option value={index + 1} key={index}>
+              {index + 1}
+            </option>
+          ))}
+        </select>
+        <ul>
+          {lessons.map((value) => {
+            const { title } = value;
+            console.log(value);
+            return <li key={title}>{title}</li>;
+          })}
+        </ul>
       </main>
     </div>
   );
 };
 
-export default LessonsPage;
+export default Lesson;
