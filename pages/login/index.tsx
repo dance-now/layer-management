08cda53e17a0386f32da522767561ccd1f31ca48
syoutarou imamura <syoutarouimamura@gmail.com>
2022-12-03 17:38:10 +0900

    パスの変更

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import { useEffect, useRef, useState } from "react";
 import { auth } from "../../src/lib/firebase";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 import {
   ConfirmationResult,
